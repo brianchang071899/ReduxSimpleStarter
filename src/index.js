c536d0378e7548ab3bdd870f3f95c1a7ef9c2405
Brian Chang <yy3chang@edu.uwaterloo.ca>
2018-09-22 17:14:32 -0400

    Started rendering videos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+
+const API_KEY = "AIzaSyCiMLJwf_KUsBGda5PW605_PwYtDDUJISk"
 
 
-const App = () => {
-  return (
-  <div>
-      <SearchBar/>
-  </div>);
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'Im a cyborg but thats ok'}, (videos) => {
+      this.setState({ videos });
+      // ^== this.setState({ videos: videos }) in ES6
+    });
+  }
+  render(){
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
